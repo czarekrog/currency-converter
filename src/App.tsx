a4cd3138ed17bdfcaf3fbd453e39f766db961a19
Cezary Róg <cezaryrog@gmail.com>
2022-11-29 15:59:57 +0100

    switching from browserRouter to hashRouter to make gh pages work

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { QueryClient } from "@tanstack/react-query";
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Home, { homeLoader } from "./routes/Home";
 import MainLayout from "./layouts/MainLayout";
 import { ResultsPage } from "./routes/ResultsPage";
@@ -12,7 +12,7 @@ export const queryClient = new QueryClient();
 const persister = createSyncStoragePersister({
   storage: window.localStorage,
 });
-const routes = createBrowserRouter([
+const routes = createHashRouter([
   {
     path: Routes.HOME,
     element: <MainLayout />,
